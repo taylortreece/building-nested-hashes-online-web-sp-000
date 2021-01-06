717c37308171f84896889f0d90bbe08ce379a393
Taylor Treece <tayloratreece@gmail.com>
2021-01-06 19:22:32 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,33 +20,11 @@ def fourth_challenge
    }
 }
 
-
-end
-
-epic_tragedy = {
- :montague => {
-    :patriarch => {name: "Lord Montague", age: "53"},
-    :matriarch => {name: "Lady Montague", age: "54"},
-    :hero => {name: "Romeo", age: "15", status: "alive"},
-    :hero_friends => [
-      {name: "Benvolio", age: "17", attitude: "worried"},
-      {name: "Mercutio", age: "18", attitude: "hot-headed"}
-    ]
- },
- :capulet => {
-    :patriarch => {name: "Lord Capulet", age: "50"},
-    :matriarch => {name: "Lady Capulet", age: "51"},
-    :heroine => {name: "Juliet", age: "15", status: "alive"},
-    :heroine_friends => [
-      {name: "Steven", age: "30", attitude: "confused"},
-      {name: "Nurse", age: "44", attitude: "worried"}
-    ]
- }
-}
-
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 return epic_tragedy
 
-puts epic_tragedy
+
+end
+
